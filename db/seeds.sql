ae5105236e389c093604654688c87c4dd4de945e
Brandon Lang <brandonlang1997@gmail.com>
2019-08-19 18:17:44 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Big Mac", false, "2019-08-11 04:20:00"),
+  ("Impossible Burger", false, "2019-08-11 04:20:01"),
+  ("Cheese Burger", false, "2019-08-11 04:20:02"),
+  ("Whopper", false, "2019-08-11 16:20:00"),
+  ("Royale with cheese", false, "2019-08-11 16:20:01"),
+  ("Crown Burger", false, "2019-08-11 04:20:20"),
+  ("American Burger", true, "2019-08-11 16:20:16");
